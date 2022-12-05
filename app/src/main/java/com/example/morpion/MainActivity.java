b3ac4260bebb02203cfecd967b766e06d6e3a92e
Brice MATIAS <bricematias3@gmail.com>
2022-12-05 11:27:15 +0100

    Add button leave application

--- app/src/main/java/com/example/morpion/MainActivity.java
+++ app/src/main/java/com/example/morpion/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.morpion;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+    }
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Voulez vous quitter l'application?")
+
+                .setTitle("Attention")
+                .setPositiveButton("Continuer", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.this.finish();
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+
+                }).show();
     }
 }
