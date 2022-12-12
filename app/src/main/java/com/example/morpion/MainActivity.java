50acc86eddf7c4f8b351f7adfa64a11c3a4606f9
macaturla <matias.caturla2@soprasteria.com>
2022-12-12 14:59:17 +0100

    Realisation d'un version test de la bd firebase

--- app/src/main/java/com/example/morpion/MainActivity.java
+++ app/src/main/java/com/example/morpion/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
     }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MorpionActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setMessage("Voulez vous quitter l'application?")
-
                 .setTitle("Attention")
                 .setPositiveButton("Continuer", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -33,4 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
                 }).show();
     }
+
+
 }
---
+++ app/src/main/java/com/example/morpion/MorpionActivity.java
@@ -0,0 +1,53 @@
+package com.example.morpion;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MorpionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morpion);
+        Button button = findViewById(R.id.button);
+        final EditText editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue(editText.getText().toString());
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("APPX", "Value is: " + value);
+                editText.setText(value);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("APPX", "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
